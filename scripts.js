94c131e64d173dc9e0d146d1fffb24a4ef782f07
cdavis19 <cjdavis6791@gmail.com>
2017-02-13 14:35:08 -0500

    initial

---
+++ scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+
+
+$('li').css('width', '200px');
+$('li').css('height', '50px');
+// $('li').fadeIn('slow', '0.25');
+
+$("li").mouseover(function(){
+    $(this).css("opacity", "0.25");
+});
+
+$("li").mouseout(function(){
+    $(this).css("opacity", "1");
+});
+
+
+
+
+
+$( "li" ).click(function() {
+  $( this ).css("background-color", "red");
+});
+
+
+
+
+$( "li" ).click(function() {
+  $( this ).text("CLICKED!!!");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ });
